8aa97d2c3fe54be09b348c4bb4aacc340b0f71b7
a-nshul <upadhyay041298@gmail.com>
2024-12-16 10:50:07 +0530

    updated code for deployement

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require("express");
-const serverless = require("serverless-http");
-const cors = require("cors");
-const connectDB = require("../config/db");
-const path = require("path");
+const cors = require("cors"); // Import the CORS middleware
+const connectDB = require("./config/db");
+const path = require('path');
 const dotenv = require("dotenv");
-const userRoutes = require("../routes/userRoutes");
-const attendanceRoutes = require("../routes/attendanceRoutes");
-const assignmentRoutes = require("../routes/assignmentRoutes");
-const { notFound, errorHandler } = require("../middleware/errorMiddleware");
+const userRoutes = require("./routes/userRoutes");
+const attendenceRoutes = require("./routes/attendanceRoutes");
+const assignmentRoutes = require("./routes/assignmentRoutes");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
+require("colors");
 
 dotenv.config();
 connectDB();
@@ -16,27 +16,31 @@ const app = express();
 app.use(express.json());
 
 // Enable CORS
-app.use(
-  cors({
-    origin: "*",
-    credentials: true,
-    allowedHeaders: ["Content-Type", "Authorization"],
-    exposedHeaders: ["X-Auth-Token"],
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  })
-);
+app.use(cors({
+  origin: "*", // Allow all origins
+  credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+  allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
+  exposedHeaders: ["X-Auth-Token"], // Expose custom headers
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Allowed methods
+  preflightContinue: false,
+  optionsSuccessStatus: 204, // For older browsers
+}));
+app.use(express.urlencoded({ extended: true }));
 
 // Define routes
 app.use("/api/user", userRoutes);
-app.use("/api/attendance", attendanceRoutes);
+app.use("/api/attendence", attendenceRoutes);
 app.use("/api/assignments", assignmentRoutes);
-
 // Serve uploaded files
-app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
-
-// Error Handling
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+// Error Handling middlewares
 app.use(notFound);
 app.use(errorHandler);
 
-// Export the serverless function
-module.exports = serverless(app);
+// Start server
+const PORT = process.env.PORT || 3001;
+
+app.listen(
+  PORT,
+  console.log(`Server running on PORT ${PORT}...`.red.bold)
+);
